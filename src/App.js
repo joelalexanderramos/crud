44ad9dbeb47e66a0943d20354ffb5a8eb2c01088
Joel Ramos <joelalexanderramos@users.noreply.github.com>
2021-05-22 23:29:17 -0400

    Delete a Task!

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { isEmpty } from 'lodash'
+import { isEmpty, size } from 'lodash'
 import React, { useState} from 'react'
 import shortid from 'shortid'
 
@@ -11,7 +11,7 @@ function App() {
     if(isEmpty(task)) {
       console.log("Task empty")
       return
-    }
+    }  
 
     const newTask = {
       id: shortid.generate(),
@@ -20,7 +20,11 @@ function App() {
 
     setTasks([...tasks, newTask])
     setTask("");
-    
+  }
+
+  const deleteTask = (id) => {
+    const filteredTasks = tasks.filter(task => task.id !== id)
+    setTasks(filteredTasks)
   }
 
   return (
@@ -31,19 +35,24 @@ function App() {
         <div className="row">
           <div className="col-8">
             <h4 className="text-center">Lista de tareas</h4>
-
-            <ul className="list-group">
-              {
-                tasks.map((task) => (
-                <li className="list-group-item" key={task.id}>
-                  <span className="lead">{task.name}</span>
-                  <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
-                  <button className="btn btn-warning btn-sm float-right">Editar</button>
-                </li>
-                ))
-              }
-
-            </ul>
+            {
+            size(tasks) === 0 ? ( <h5  className="text-center">Aún No hay tareas programadas.</h5>) : (
+              <ul className="list-group">
+                {
+                  tasks.map((task) => (
+                  <li className="list-group-item" key={task.id}>
+                    <span className="lead">{task.name}</span>
+                    <button 
+                      className="btn btn-danger btn-sm float-right mx-2" 
+                      onClick={ () => deleteTask(task.id) }>
+                        Eliminar</button>
+                    <button className="btn btn-warning btn-sm float-right">Editar</button>
+                  </li>
+                  ))
+                }
+              </ul>
+              )
+            }
           </div>
 
           <div className="col-4">
